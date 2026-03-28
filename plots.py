0b74c860ea9f8b705d5705660acc1c7c8ef8fc4e
PensiveEagle <halljf96@gmail.com>
2026-03-28 10:47:23 +0000

    Added type hint for generate plot func

--- plots.py
+++ plots.py
@@ -1,8 +1,9 @@
 import data_functions
 import plotly.express as px
+from plotly.graph_objs._figure import Figure as plotly_figure
 import pandas as pd
 
-def generate_line_plot( data: pd.DataFrame, x_column: str, y_column: str ):
+def generate_line_plot( data: pd.DataFrame, x_column: str, y_column: str ) -> plotly_figure:
     plot =px.line( data_frame = data, x = data[x_column], y = data[y_column], labels = { "x": x_column, "y": y_column } )
     return plot
     
