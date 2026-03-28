a940be1399b8b9b9ae065545b1f8dc0d33cbe02c
PensiveEagle <halljf96@gmail.com>
2026-03-28 12:58:48 +0000

    Added function to generate a scatter plot

--- plots.py
+++ plots.py
@@ -6,10 +6,15 @@ import pandas as pd
 def generate_line_plot( data: pd.DataFrame, x_column: str, y_column: str ) -> plotly_figure:
     plot =px.line( data_frame = data, x = data[x_column], y = data[y_column], labels = { "x": x_column, "y": y_column } )
     return plot
+
+def generate_scatter_plot( data: pd.DataFrame, x_column: str, y_column: str ) -> plotly_figure:
+    plot =px.scatter( data_frame = data, x = data[x_column], y = data[y_column], labels = { "x": x_column, "y": y_column } )
+    return plot
     
 if __name__ == "__main__":
     import streamlit as st
-    line_chart = generate_line_plot( data_functions.get_data_from_csv(), "happiness", "gdp" )
-    print( type( line_chart ) )
+    line_chart = generate_line_plot( data_functions.get_data_from_csv(), "happiness", "gdp" )#
+    scatter_chart = generate_scatter_plot( data_functions.get_data_from_csv(), "happiness", "gdp" )
     st.title( "test chart" )
     st.plotly_chart( line_chart )
+    st.plotly_chart( scatter_chart )
